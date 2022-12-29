bef2b533107b73e38bc4199bb4992beb2e5143a0
viratsathish1999 <sathish.r9688@gmail.com>
2022-12-29 22:08:09 +0530

    add file

---
+++ main.js
@@ -0,0 +1,10 @@
+function increaseValue(){
+  let value=document.getElementById("value").value;
+  value++;
+  document.getElementById("value").value=value;
+}
+function decreaseValue(){
+  let value=document.getElementById("value").value;
+  value--;
+  document.getElementById("value").value=value;
+}
